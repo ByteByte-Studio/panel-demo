ec29e102b1233689c1b529c9fc4b424b4a8caa36
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-04-26 22:25:29 -0700

    add: factories

---
+++ database/factories/ConversationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Conversation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Conversation>
+ */
+class ConversationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'chat_id' => $this->faker->numerify('521##########'),
+            'user_name' => $this->faker->name(),
+        ];
+    }
+}
