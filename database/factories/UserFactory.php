65b773b5b6230b7a18f32f320f8a1a9ac6e507d4
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-04-28 17:03:10 -0700

    add: user model enhancements and filament resource

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,30 +12,23 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone_number' => fake()->phoneNumber(),
+            'whatsapp_number' => fake()->numerify('521##########'),
+            'avatar_url' => 'https://i.pravatar.cc/150?u='.fake()->uuid(),
+            'bio' => fake()->sentence(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
